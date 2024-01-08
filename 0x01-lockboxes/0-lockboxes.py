4b66f9fd4406501a790911a40d7269cf084a67ce
Peter Ekwere <peterekwere04@gmail.com>
2024-01-08 03:35:22 -0800

    initial commit for task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+"""
+    This Module Contains the LockBox Practice
+    Author: Peter Ekwere
+"""
+
+
+def canUnlockAll(boxes):
+    """
+        This is the CanUnlockALL Function
+    """
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True  # The first box is unlocked
+
+    stack = [0]  # Start with the first box
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
