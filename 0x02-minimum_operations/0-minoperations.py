6ca25c52a1e724a5addcf23163891a83932a1faf
PeterEkwere <peterekwere@icloud.com>
2024-01-18 13:57:13 +0100

    initial commit for task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    This Module contains Minimum Operations interview practice
+    Author: Peter Ekwere
+"""
+
+
+def copy_all(n: str) -> str:
+    """ This function multiplies a string 
+    """
+    return n * 2
+ 
+    
+def minOperations(n: int) -> int:
+    """ This function calculates the fewest number of operations needed to result in exactly n H characters
+    
+
+    Args:
+        n (int): n is the amount of H characters
+
+    Returns:
+        int: the fewest of operations
+    """
+    operations_count = 0
+    characters = ""
+    
+    for i in range(n):
+        if len(characters) == n:
+            return operations_count
+        elif len(characters) < n:
+            if len(characters) < 1:
+                characters = "H"
+            if (len(characters) * 2) < n:
+                characters = copy_all(characters)
+                operations_count += 1
+            else:
+                characters += "H"
+                operations_count += 1
+            
