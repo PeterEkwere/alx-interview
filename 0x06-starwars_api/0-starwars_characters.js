7f471c9869001753ee1b0a9b743b30330dc1a251
Peter Ekwere <peterekwere04@gmail.com>
2024-02-14 04:55:25 -0800

    update 0 for task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,13 +3,12 @@
 // Import the request module
 const request = require('request');
 const args = process.argv;
-
 const arg1 = args[2];
 
 // Define the URL you want to make a request to
 const url = 'https://swapi-api.alx-tools.com/api/films/' + arg1 + '/';
 
-// Make a GET request
+// Make a GET request to fetch film data
 request.get(url, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
@@ -21,8 +20,14 @@ request.get(url, (error, response, body) => {
   // Extract characters array
   const characters = filmData.characters;
 
-  // Iterate over characters array
-  characters.forEach(characterUrl => {
+  // Function to fetch character data recursively and display names
+  const fetchAndDisplayCharacterNames = (index) => {
+    if (index >= characters.length) {
+      return; // Base case: if all characters are processed, stop recursion
+    }
+
+    const characterUrl = characters[index];
+
     // Make a GET request to each character URL
     request.get(characterUrl, (error, response, body) => {
       if (error) {
@@ -35,6 +40,12 @@ request.get(url, (error, response, body) => {
 
       // Extract and log the name of each character
       console.log(characterData.name);
+
+      // Recursive call to fetch and display next character's name
+      fetchAndDisplayCharacterNames(index + 1);
     });
-  });
+  };
+
+  // Start fetching and displaying character names
+  fetchAndDisplayCharacterNames(0);
 });
